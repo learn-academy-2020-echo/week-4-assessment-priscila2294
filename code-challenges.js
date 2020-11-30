4a35be01aea8e05cccafc9bd59280d1f9d8753ba
priscila2294 <priscilla_2294@hotmail.com>
2020-11-30 06:28:34 -0800

    initial commit

--- code-challenges.js
+++ code-challenges.js
@@ -7,16 +7,63 @@ var collections = ["purple", "blue", "green", "yellow", "pink"]
 // Expected output example2 (can be a different order): ["blue", "green", "pink"]
 // Expected output example3 (can be a different order): ["pink", "green"]
 
+// Very very very confusing! I started by creating a funcion that takes an array as an argument.
+const shuffle = (array) => {
+    //if array is empty
+// Then I set my if statement when my array is empty, you can set it equal to 0, [],or ""
+    if (array=== 0) {
+        return "The array is empty."
+// Now I can start working on the very very very confusing part.
+// Start by using .shift(), this method removes the first element from an array and returns that removed element when called, but we don't want that.
+    } else {
+        array.shift()
+// Set a basic for lop, 
+        for (let i=0; i<array.length; i++) {
+// Now the hard part-creating a local variable that will find a random index and suffle my array. 
+// Math.floor(Math.random()) will return a random integer from my array.
+// https://dirask.com/posts/JavaScript-Math-random-explain-the-difference-between-Math-ceil-vs-Math-floor-vs-Math-round-aDZQd1
+                randIndex = Math.floor(Math.random()*array.length)
+// Now this is actually the hard part...
+//switch the position of value at random index with the value at i
+// create a new local variable that will hold my array and randInex
+                temp = array[randIndex]
+                array[randIndex] = array[i]
+                array[i] = temp
+            }
+        return array
+    
+    }
+}
 
-
+// console.log(shuffle(collections))
+// console.log(shuffle(collections))
+// console.log(shuffle(collections))
+// console.log(shuffle(collections))
+// console.log(shuffle(collections))
 
 
 // --------------------2) Create a function that takes in an array of numbers and returns the sum of all the numbers cubed.
+
 
 var cubeAndSum1 = [2, 3, 4]
 // Expected output: 99
 var cubeAndSum2 = [0, 5, 10]
 // Expected output: 1125
+// Start by creating a function that takes in an array as an argument
+
+const cubeNumbs = (array) => {
+// create a local variable that holds out incomming data
+    let start = 0
+// use a map to go through our array and cube each value, then add those values together
+    array.map(value => {
+
+     start = start + value **3 
+}) 
+
+return start
+}
+
+// console.log(cubeNumbs(cubeAndSum1));
 
 
 
@@ -29,11 +76,30 @@ var nums1 = [3, 56, 90, -8, 0, 23, 6]
 var nums2 = [109, 5, 9, -59, 8, 24]
 // Expected output: [-59, 109]
 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/min
+// so far my favorite, love this method
+
+const myFunction = (array) => {
+    let max = (Math.min(...array))
+    let min = (Math.max(...array))
+    return [max,min]
+}  
+
+var nums1 = [3, 56, 90, -8, 0, 23, 6]
+// Expected output: [-8, 90]
+var nums2 = [109, 5, 9, -59, 8, 24]
+// Expected output: [-59, 109]
+
+// console.log(myFunction(nums1))
+// console.log(myFunction(nums2))
+
+
 
 
 
 
 // --------------------4) Create a function that takes in a string and returns a string with every other letter capitalized.
+// https://stackoverflow.com/questions/43192205/making-every-other-letter-capitalized
 
 var testString1 = "albatross"
 // Expected output: "aLbAtRoSs"
@@ -41,7 +107,18 @@ var testString2 = "jabberwocky"
 // Expected output: "jAbBeRwOcKy"
 
 
+const firstLettUpp = (string) => {
+  let output = ""
+  for (i=0; i < string.length; i++) {
+     output  += ! (i % 2 == 0) ? string.charAt(i).toUpperCase() : string.charAt(i);
+  }
+  return output 
+}
 
+
+// console.log(firstLettUpp(testString1))
+
+// console.log(firstLettUpp(testString2))
 
 
 // --------------------5) Create a function that takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator to pass in a dynamic number of arguments.
@@ -49,3 +126,13 @@ var testString2 = "jabberwocky"
 var arr1 = [3, 7, 10, 5, 4, 3, 3]
 var arr2 = [7, 8, 2, 3, 1, 5, 4]
 // Expected output: [3, 7, 10, 5, 4, 8, 2, 1]
+
+// create a function that takes two arrays as an argument and return one array with the two arrays combined and no duplicates 
+
+const noDupes = (arr1, arr2) => {
+    // lets create a local variable that will hold our new arrat
+    let singleArray = new Set ([...arr1,...arr2]) 
+    
+    return [...singleArray]
+}
+// console.log(noDupes(arr1,arr2));
--- code_challenges.rb
+++ code_challenges.rb
@@ -9,6 +9,15 @@ full_arr2 = ['hello', 7, 23, -823, false, 78, nil, '67', 6, 'Number']
 # Expected output: [-823, 7, 23]
 
 
+#Create a method that will take in an array and return one with only odd numbers.
+def odd_array array
+    new_array = array.select { |value| value.is_a?(Integer) && value % 2 != 0}
+    print new_array.sort
+end
+
+
+# odd_array full_arr1
+# odd_array full_arr2
 
 
 
@@ -20,11 +29,31 @@ letter_o = 'o'
 letter_a = 'a'
 # Expected output: ['tea', 'soda water']
 
+# https://stackoverflow.com/questions/26265392/finding-an-objects-within-an-array-with-specific-letter-using-ruby
+# thank you stackoverflow
+# You could write more simple way -
 
+# def finder(array, thing_to_find)
+#   array.select { |word| word.include? thing_to_find }
+# end
+# array will pass each element word from that array to the block in each iteration. Now word.include?(thing_to_find) will be evaluated - If it returns true, then word will be selected and stored to an temporary array, otherwise not. Once all iterations will be completed with respected to the array, then all selected elements from each block execution, which were being stored inside a temporary array, that array will be returned.
+
+
+
+def finder (array, letter_to_find)
+    array.select { |word| word.include? letter_to_find }
+end
+
+
+# p finder beverages_array, letter_a
+# p finder beverages_array, letter_o
 
 
 
 # -------------------3) Create a method that takes in a string and removes all the vowels from the string. Use the test variables provided. HINT: Check out this resource: https://ruby-doc.org/core-2.6/String.html#method-i-delete
+# def remvowel(string)
+#     string.delete 'aeiou'
+#   end
 
 album1 = 'Rubber Soul'
 # Expected output: 'Rbbr Sl'
@@ -33,17 +62,35 @@ album2 = 'Sgt Pepper'
 album3 = 'Abbey Road'
 # Expected output: 'bby Rd'
 
+def disemvowel string
+    string.delete 'A,E,I,O,U,a,e,i,o,u'
+end
+
+# p disemvowel album1
+# p disemvowel album2
+# p disemvowel album3
+
 
 
 
 
 # -------------------4) Create a method that takes in an array of numbers and returns the sum of the numbers. Use the test variables provided.
+# https://stackoverflow.com/questions/1538789/how-to-sum-array-of-numbers-in-ruby
+
+# https://ruby-doc.org/core-2.7.2/Enumerable.html#method-i-inject
+
 
 nums_array1 = [42, 7, 27]
 # Expected output: 76
 
-nums_array1 = [25, 17, 47, 11]
+nums_array2 = [25, 17, 47, 11]
 # Expected output: 100
+
+def sum_nums array
+    array.inject(0){|sum,value| sum + value}
+end
+# p sum_nums nums_array1
+# p sum_nums nums_array2
 
 
 
